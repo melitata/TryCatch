f0c9f52da2d224567ca5fbfac7f4bcd390771f1c
Melisa Kılıç <mk_i3@hotmail.com>
2024-11-04 20:59:34 +0300

    Proje dosyası ekle.

---
+++ TryCatch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Lütfen bir sayı girin: ");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+            int square = number * number;
+            Console.WriteLine("Girdiğiniz sayının karesi: " + square);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Geçersiz giriş! Lütfen daha küçük veya daha büyük bir sayı giriniz.");
+        }
+    }
+}
